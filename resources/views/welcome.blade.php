4a4f08aa9622efacc38d67cf0fbf970e21bac3df
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-19 13:46:06 +0100

    Add Target for Digital Ocean

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,17 @@
 </head>
 <body class="bg-grey-lighter">
 <div class="container mx-auto">
+    <span class="text-center w-auto">
+        <div class="bg-red-lightest border border-red-light text-red-dark px-4 py-3 rounded relative" role="alert">
+  <strong class="font-bold">Attention!</strong>
+  <span class="block sm:inline">This monitoring is still work in progress and hasn't enough data to be valid!</span>
+  <span class="absolute pin-t pin-b pin-r px-4 py-3">
+    <svg class="fill-current h-6 w-6 text-red" role="button" xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 20 20"><title>Close</title><path
+                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  </span>
+</div>
+    </span>
     <div class="w-auto h-100 text-center">
         <h3>Response Time of the Servers List API Endpoint</h3>
         <canvas id="hetzner_cloud_api_response_time"></canvas>
