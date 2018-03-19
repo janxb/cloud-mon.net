1c3e4a122e9a426ed635f59239b8c3bd5fbde351
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-19 11:05:07 +0100

    Make the Tests every hour

--- resources/views/provider/edit.blade.php
+++ resources/views/provider/edit.blade.php
@@ -10,6 +10,7 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('provider.update',$provider) }}">
                             {{ csrf_field() }}
+                            {{ method_field('PUT') }}
                             <div class="form-group">
                                 <label>Name</label>
                                 <input type="text" class="form-control" name="name" value="{{ $provider->name }}">
