b212330e7d637be9ed567dd5c8db1164a0de7e08
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-19 10:55:23 +0100

    Initial Commit

--- app/Targets/HetznerTarget.php
+++ app/Targets/HetznerTarget.php
@@ -50,7 +50,7 @@ class HetznerTarget extends AbstractTarget
         $location = $locations->get(1);
         $ssh_keys = new SSHKeys();
 
-        $created_server = $server->create('mon-cloud-test-hetzner.mon-cloud.net', $serverType, $image, $location, null, [$ssh_keys->get(18802)]);
+        $created_server = $server->create('mon-cloud-test-hetzner.mon-cloud.net', $serverType, $image, $location, null, [18802]);
         $start = microtime(true);
         $ping = new Ping($created_server->publicNet->ipv4->ip, 255, 5);
         $trys = 100;
