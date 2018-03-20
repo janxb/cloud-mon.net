dedc4a37dc9a21fb8d948b49f0b8a7104ecbcbb3
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-03-20 08:17:02 +0100

    Make Visible only from the last 12 hours

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,18 +108,18 @@
     var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels:{!! json_encode(\App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->get()->map(function($check){
+            labels:{!! json_encode(\App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->orderBy('id','DESC')->get()->map(function($check){
         return  $check->created_at->format('d.m.Y h\:00 a');
        })) !!},
             datasets:
             {!! json_encode(\App\Models\Provider::all()->map(function($provider){
-           $data = $provider->checks()->where('check','=','server_creation_time')->limit(12)->get()->map(function($check){
+           $data = $provider->checks()->where('check','=','server_creation_time')->limit(12)->orderBy('id','DESC')->get()->map(function($check){
     return [
     'x' => $check->created_at->format('d.m.Y H:i:s'),
     'y' => (float) $check->result
     ];})->toArray();
-    if(count($data) < \App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->count()){
-    $diff = \App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->count() - count($data);
+    if(count($data) < \App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->orderBy('id','DESC')->count()){
+    $diff = \App\Models\Provider::find(1)->checks()->where('check','=','server_creation_time')->limit(12)->orderBy('id','DESC')->count() - count($data);
 
     for($i = 0; $i < $diff; $i++){
         array_unshift($data,[
@@ -165,18 +165,18 @@
     var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels:{!! json_encode(\App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->get()->map(function($check){
+            labels:{!! json_encode(\App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->orderBy('id','DESC')->get()->map(function($check){
         return  $check->created_at->format('d.m.Y h\:00 a');
        })) !!},
             datasets:
             {!! json_encode(\App\Models\Provider::all()->map(function($provider){
-               $data = $provider->checks()->where('check','=','api_response_time')->limit(12)->get()->map(function($check){
+               $data = $provider->checks()->where('check','=','api_response_time')->limit(12)->orderBy('id','DESC')->get()->map(function($check){
     return [
     'x' => $check->created_at->format('d.m.Y H:i:s'),
     'y' => (float) $check->result
     ];})->toArray();
-             if(count($data) < \App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->count()){
-    $diff = \App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->count() - count($data);
+             if(count($data) < \App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->orderBy('id','DESC')->count()){
+    $diff = \App\Models\Provider::find(1)->checks()->where('check','=','api_response_time')->limit(12)->orderBy('id','DESC')->count() - count($data);
 
     for($i = 0; $i < $diff; $i++){
         array_unshift($data,[
