c8bc88810837792e68a76972b6ad9a03ae702623
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-04-23 09:30:00 +0200

    Improve mobile navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,14 +23,14 @@
         <span class="font-semibold text-xl tracking-tight">cloud-mon.net</span>
     </div>
     <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white" onclick="toggleNav()">
             <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>
                     Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
             </svg>
         </button>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="w-full block sm:invisible flex-grow lg:flex lg:items-center lg:w-auto" id="nav">
         <div class="text-sm lg:flex-grow">
             <a href="#api_response_time"
                class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
@@ -245,6 +245,14 @@
             });
         });
     });
+    
+    function toggleNav(){
+        if($('#nav').hasClass('sm:invisible')){
+            $('#nav').removeClass('sm:invisible');
+        } else {
+            $('#nav').addClass('sm:invisible');
+        }
+    }
 </script>
 </body>
 </html>
